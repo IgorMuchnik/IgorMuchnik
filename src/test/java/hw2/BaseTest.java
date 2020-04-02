10c14b71a1741c3930b3afa0fbff6591797640d9
mu4nik <muchnik.up@gmail.com>
2020-04-02 13:10:06 +0300

    added bonigarcia/webdrivermanager to handle with drivers

--- src/test/java/hw2/BaseTest.java
+++ src/test/java/hw2/BaseTest.java
@@ -1,8 +1,8 @@
 package hw2;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -19,6 +19,7 @@ public class BaseTest {
 
   @BeforeMethod
   public void setUp() {
+    WebDriverManager.chromedriver().setup();
     driver = new ChromeDriver();
     wait = new WebDriverWait(driver, 5);
     softAssert = new SoftAssert();
